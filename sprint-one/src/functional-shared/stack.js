7b5ba9e64e7a37858c2caf8e3c8799a4e584884d
Amber <amber.j.nola@gmail.com>
2019-09-18 18:38:29 -0500

    finished shared functionality

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,41 +1,54 @@
+// Functional instantiation with shared methods: same as step 1, but with shared methods
+// Do:
+// Work within the src / functional - shared / folder
+// Create an object that holds the methods that will be shared by all instances of the class
+//   Use the keyword this in your methods
+// Use _.extend to copy the methods onto the instance
+// Don’t: Use the keyword new or prototype chains
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-
-  // Use the keyword this in your methods
-  // Use _.extend to copy the methods onto the instance
-    //takes arguments and adds its properties to the first argument
-
-  let someInstance = {};
-  //create a container, some instance {}
-  let storage = {}; 
+  // let  = {};
   //create a storage container//cache
-  let size = 0;
+  let newStack = {};
+  newStack.storage = {};
+  newStack.length = 0; 
   //created a size increimetor 
+  _.extend(newStack, stackMethods);
 
-  //add a push method
-    //
-    someInstance.push = function(value) {
-      this.storage[this.size] = value; 
-      this.size++; 
-      //value is a string
-
-    };
-
-  //add a pop method
-  someInstance.pop = function() {
-    this.size--; 
-    //decrease size
-  }
-
-  //add a size method 
-  someInstance.size = function() {
-    //return size; 
-    return this.size; 
-  }
-
-
+  return newStack; 
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.length] = value; 
+  this.length++; 
+};
+  
+
+//add a pop method
+stackMethods.pop = function() {
+
+  if (this.size() > 0) {
+    //save result of stroage place 
+    var result = this.storage[this.length - 1];
+    //delete it from storage
+    delete this.storage[this.length - 1];
+
+    this.length--;
+    //return the saved result 
+    return result;
+
+  } else {
+    return null;
+  }
+};
+
+  
+//add a size method 
+stackMethods.size = function() {
+  //return size; 
+  return this.length; 
+};
