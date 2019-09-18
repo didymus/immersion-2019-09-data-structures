7a404f6bd11e055a1ae7d4232255c2e8e6f19445
Amber <amber.j.nola@gmail.com>
2019-09-18 16:25:30 -0500

    stack in works

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  let size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size === 0) {
+      return size; 
+    }
+    size--;
   };
 
   someInstance.size = function() {
+    return size; 
+
   };
 
   return someInstance;
