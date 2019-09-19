f335d23566238fd00bdd0e48ac7afb3786aa1605
Amber <amber.j.nola@gmail.com>
2019-09-19 12:26:46 -0500

    pseudocode on LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,9 +1,22 @@
+// A linkedList class, in functional style, with the following properties:
+// [].head property, a linkedListNode instance
+// [].tail property, a linkedListNode instance
+// [].addToTail() method, takes a value and adds it to the end of the list
+// [].removeHead() method, removes the first node from the list and returns its value
+// [].contains() method, returns boolean reflecting whether or not the passed -in value is in the linked list
+// [] What is the time complexity of the above functions ?
+
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    //add value to end of list
+    //assign tail the last value. 
+    list[Node(value)];
+    list.tail = value;
   };
 
   list.removeHead = function() {
@@ -15,10 +28,12 @@ var LinkedList = function() {
   return list;
 };
 
+//node constructor function
 var Node = function(value) {
   var node = {};
 
   node.value = value;
+  //points to the next linked node
   node.next = null;
 
   return node;
