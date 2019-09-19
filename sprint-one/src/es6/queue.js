171bd4c874a0731273eeec450f38b5fee0fea29d
Amber <amber.j.nola@gmail.com>
2019-09-18 20:03:42 -0500

    finished sprint 1

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,7 +2,31 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {}; 
+    this.head = 0;
+    this.tail = 0; 
   }
+
+  enqueue(value) {
+    this.storage[this.tail] = value;
+    this.tail++;
+  };
+
+  dequeue() {
+    if (this.head <= this.tail) {
+      let result = this.storage[this.head];
+      delete this.storage[this.head];
+      this.head++;
+      return result;
+    }
+  };
+
+  size() {
+    if (this.tail - this.head <= 0) {
+      return 0;
+    }
+    return this.tail - this.head;
+  };
 
 }
 
--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,5 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}; 
+  this.head = 0;
+  this.tail = 0; 
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.tail] = value; 
+  this.tail++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.head <= this.tail) {
+    let result = this.storage[this.head];
+    delete this.storage[this.head];
+    this.head++;
+    return result; 
+  }
+};
+
+Queue.prototype.size = function() {
+  if (this.tail - this.head <= 0) {
+    return 0;
+  }
+  return this.tail - this.head;
+};
